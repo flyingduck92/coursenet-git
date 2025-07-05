598b88a2102f8d5e3d86f255c6ca8e387d7522df
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-05 13:59:43 +0700

    Add algorithm part above pseudocode

--- algorithm_pseudocode.js
+++ algorithm_pseudocode.js
@@ -1,6 +1,14 @@
 /* 
   CASE 1 - RECTANGLE AREA
 
+  - ALGORITHM
+    1. make variable panjang with any value
+    2. make variable lebar with any value
+    3. make variable area with lebar MULTIPLY BY panjang
+    4. display the result of area
+
+  - PSEUDOCODE 
+
   BEGIN 
     STORE panjang WITH 10
     STORE lebar WITH 5
