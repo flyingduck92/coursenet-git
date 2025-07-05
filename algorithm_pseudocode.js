c35881a509072a4d79d501bead5e999de0136d23
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-05 15:13:19 +0700

    Add surface aread of cuboid

--- algorithm_pseudocode.js
+++ algorithm_pseudocode.js
@@ -67,11 +67,10 @@
     1. make variable constant PHI with 3.14
     2. make variable radius (r) with any value
     3. make variable height (h) with any value
-    
     4. make CIRCLE_AREA with phi MULTIPLY BY SQUARED of radius(r*r)
     5. make CURVED_AREA with 2 MULTIPLY phi MULTIPLY radius(r) MULTIPLY height(h)
-    
     6. make ALL_SURFACE with 2 MULTIPLY circle_area ADD curved_area 
+    7. display ALL_SURFACE
 
   - PSEUDOCODE 
     
@@ -86,6 +85,37 @@
     STORE all_surface WITH 2 MUL circle_area ADD curved_area
 
     SHOW all_surface
+  END
+  
+*/
+
+/* 
+  CASE 5 - SURFACE AREA OF CUBOID
+
+  - ALGORITHM ()
+    1. make variable length(l)
+    2. make variable width(w)
+    3. make variable height(h)
+    4. make top_bottom_face WITH length(l) MULTIPLY width(w) MULTIPLY 2
+    5. make right_left_face WITH width(w) MULTIPLY height(h) MULTIPLY 2 
+    6. make front_back_face WITH length(l) MULTIPLY height(h) MULTIPLY 2 
+    7. make all_faces WITH top_bottom_face ADD right_left_face ADD front_back_face
+    8. display all_faces
+    
+  - PSEUDOCODE 
+    
+  BEGIN
+    STORE l WITH ANY NUMBER (4)
+    STORE w WITH ANY NUMBER (5)
+    STORE h WITH ANY NUMBER (10)
+
+    STORE top_bottom_face WITH l MUL w MUL 2
+    STORE right_left_face WITH w MUL h MUL 2
+    STORE front_back_face WITH l MUL h MUL 2
+
+    STORE all_faces WITH top_bottom_face ADD right_left_face ADD front_back_face
+
+    SHOW all_faces
   END
   
 */
