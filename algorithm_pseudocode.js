2ce73822b6dbb228a23ca9ae099133d614f98ca2
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-05 14:29:49 +0700

    add volume of half-sphere and notes update

--- algorithm_pseudocode.js
+++ algorithm_pseudocode.js
@@ -38,3 +38,24 @@
   END
   
 */
+
+/* 
+  CASE 3 - VOLUME OF HALF-SPHERE 
+
+  - ALGORITHM
+    1. make variable constant PI with 3.14
+    2. make variable radius (r)
+    3. make variable valume with (2/3) MULTIPLIED BY pi MULTIPLIED BY CUBE of radius(r*r*r)
+    4. display the result of volume
+
+  - PSEUDOCODE 
+    
+  BEGIN
+    STORE pi WITH 3.14
+    STORE r WITH ANY NUMBER (5)
+    STORE volume WITH (2/3) MULTIPLIED BY pi MULTIPLIED BY CUBE(r)
+
+    SHOW volume
+  END
+  
+*/
