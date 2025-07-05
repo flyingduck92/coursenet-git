89385129aa6026f03470d7809d1b04a62a93d06f
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-05 14:14:56 +0700

    Add area of circle

--- algorithm_pseudocode.js
+++ algorithm_pseudocode.js
@@ -10,10 +10,31 @@
   - PSEUDOCODE 
 
   BEGIN 
-    STORE panjang WITH 10
-    STORE lebar WITH 5
+    STORE panjang WITH ANY NUMBER (10)
+    STORE lebar WITH ANY NUMBER (5)
 
-    STORE area WITH panjang MULTIPLY lebar
+    STORE area WITH panjang MULTIPLIED lebar
     SHOW area
   END
+*/
+
+/* 
+  CASE 2 - AREA OF CIRCLE
+
+  - ALGORITHM
+    1. make variable constant PI with 3.14
+    2. make variable radius (r)
+    3. make variable area with PI MULTIPLIED BY SQUARED of radius (r*r)
+    4. display the result of area
+
+  - PSEUDOCODE 
+    
+  BEGIN
+    STORE pi WITH 3.14
+    STORE r WITH ANY NUMBER (5)
+    STORE area WITH pi MULTIPLIED BY SQUARED(r)
+
+    SHOW area
+  END
+  
 */
