5ad0a4d5dd07f41dfaaa6b2d73cff31e8a6b03c3
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-05 14:41:35 +0700

    changed squared and cube to MUL for better pseudocode insight and update pi to PHI

--- algorithm_pseudocode.js
+++ algorithm_pseudocode.js
@@ -13,7 +13,7 @@
     STORE panjang WITH ANY NUMBER (10)
     STORE lebar WITH ANY NUMBER (5)
 
-    STORE area WITH panjang MULTIPLIED lebar
+    STORE area WITH panjang MUL lebar
     SHOW area
   END
 */
@@ -22,7 +22,7 @@
   CASE 2 - AREA OF CIRCLE
 
   - ALGORITHM
-    1. make variable constant PI with 3.14
+    1. make variable constant PHI with 3.14
     2. make variable radius (r)
     3. make variable area with PI MULTIPLIED BY SQUARED of radius (r*r)
     4. display the result of area
@@ -30,9 +30,9 @@
   - PSEUDOCODE 
     
   BEGIN
-    STORE pi WITH 3.14
+    STORE phi WITH 3.14
     STORE r WITH ANY NUMBER (5)
-    STORE area WITH pi MULTIPLIED BY SQUARED(r)
+    STORE area WITH pi MUL r MUL r
 
     SHOW area
   END
@@ -43,7 +43,7 @@
   CASE 3 - VOLUME OF HALF-SPHERE 
 
   - ALGORITHM
-    1. make variable constant PI with 3.14
+    1. make variable constant PHI with 3.14
     2. make variable radius (r)
     3. make variable valume with (2/3) MULTIPLIED BY pi MULTIPLIED BY CUBE of radius(r*r*r)
     4. display the result of volume
@@ -51,9 +51,9 @@
   - PSEUDOCODE 
     
   BEGIN
-    STORE pi WITH 3.14
+    STORE phi WITH 3.14
     STORE r WITH ANY NUMBER (5)
-    STORE volume WITH (2/3) MULTIPLIED BY pi MULTIPLIED BY CUBE(r)
+    STORE volume WITH (2/3) MUL phi MUL r MUL r MUL r
 
     SHOW volume
   END
