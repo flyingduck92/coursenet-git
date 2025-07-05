d86644288609df021713fb9275a64f40c6d00063
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-05 15:00:47 +0700

    add surface area of culinder

--- algorithm_pseudocode.js
+++ algorithm_pseudocode.js
@@ -24,7 +24,7 @@
   - ALGORITHM
     1. make variable constant PHI with 3.14
     2. make variable radius (r)
-    3. make variable area with PI MULTIPLIED BY SQUARED of radius (r*r)
+    3. make variable area with phi MULTIPLIED BY SQUARED of radius (r*r)
     4. display the result of area
 
   - PSEUDOCODE 
@@ -32,7 +32,7 @@
   BEGIN
     STORE phi WITH 3.14
     STORE r WITH ANY NUMBER (5)
-    STORE area WITH pi MUL r MUL r
+    STORE area WITH phi MUL r MUL r
 
     SHOW area
   END
@@ -45,7 +45,7 @@
   - ALGORITHM
     1. make variable constant PHI with 3.14
     2. make variable radius (r)
-    3. make variable valume with (2/3) MULTIPLIED BY pi MULTIPLIED BY CUBE of radius(r*r*r)
+    3. make variable valume with (2/3) MULTIPLIED BY phi MULTIPLIED BY CUBE of radius(r*r*r)
     4. display the result of volume
 
   - PSEUDOCODE 
@@ -56,6 +56,36 @@
     STORE volume WITH (2/3) MUL phi MUL r MUL r MUL r
 
     SHOW volume
+  END
+  
+*/
+
+/* 
+  CASE 4 - SURFACE AREA OF CYLINDER
+
+  - ALGORITHM (curved surface area + 2 circular bases)
+    1. make variable constant PHI with 3.14
+    2. make variable radius (r) with any value
+    3. make variable height (h) with any value
+    
+    4. make CIRCLE_AREA with phi MULTIPLIED BY SQUARED of radius(r*r)
+    5. make CURVED_AREA with 2 MULTIPLY phi MULTIPLY radius(r) MULTIPLY height(h)
+    
+    6. make ALL_SURFACE with 2 MULTIPLY circle_area ADD curved_area 
+
+  - PSEUDOCODE 
+    
+  BEGIN
+    STORE phi WITH 3.14
+    STORE r WITH ANY NUMBER (5)
+    STORE h WITH ANY NUMBER (10)
+
+    STORE circle_area WITH phi MUL r MUL r
+    STORE curved_area WITH 2 MUL phi MUL r MUL h
+
+    STORE all_surface WITH 2 MUL circle_area ADD curved_area
+
+    SHOW all_surface
   END
   
 */
