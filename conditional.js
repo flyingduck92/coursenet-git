a3e5363bfcb9e77d0bacba96a73590d3874d4deb
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-06 11:57:33 +0700

    if condition and swap value

---
+++ conditional.js
@@ -0,0 +1,15 @@
+let age = 17
+
+if (age >= 17) {
+  console.log('Bisa bikin KTP')
+} else {
+  console.log('Belum bisa bikin KTP')
+}
+
+
+// swapped number
+let a = 100
+let b = 20
+console.table({ a, b });
+[a, b] = [b, a]
+console.table({ a, b })
