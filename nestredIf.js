f2d4f8cb9959edef8cc8bdbadfea0112e5fbe76b
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-06 13:27:29 +0700

    nested if in jscode

---
+++ nestredIf.js
@@ -0,0 +1,13 @@
+
+let height = 155
+let age = 17
+
+if (age < 17) {
+  if (height < 150) {
+    console.log('Tidak boleh masuk')
+  } else {
+    console.log('Boleh masuk')
+  }
+} else {
+  console.log('Maaf, anda sudah dewasa')
+}
