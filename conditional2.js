01b5dc7c74c72a859eda8e95b5891797094ffa04
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-06 14:11:11 +0700

    Switch case practise

--- conditional2.js
+++ conditional2.js
@@ -29,3 +29,55 @@ if (myNumber % 2 === 0) {
   console.log(`myNumber (${myNumber}) is Odd`)
 }
 
+/*
+  CASE 2 - SWITCH CASE 
+  A Vending Machine could accept a string input and gives drinks the following:
+  1. Sweet Tea
+  2. Plain Tea
+  3. Boba
+  4. Milk Coffee
+  5. Choco Tea
+  If the buyer choose Sweet Tea, it will gives an output "You choose sweet tea. Thanks." 
+  Please make the algorithm or pseudocode and JS code
+
+  - PSEUDOCODE
+  BEGIN 
+    STORE chosen_drink WITH ANY STRING
+
+    IF chosen_drink EQUAL "Sweet Tea"
+      SHOW "You choose Sweet Tea. Thanks." 
+    ELSE IF chosen_drink EQUAL "Plain Tea
+      SHOW "You choose Plain Tea." 
+    ELSE IF chosen_drink EQUAL "Boba"
+      SHOW "You choose Boba. Thanks." 
+    ELSE IF chosen_drink EQUAL "Milk Coffee"
+      SHOW "You choose Milk Coffee. Thanks." 
+    ELSE IF chosen_drink EQUAL "Choco Tea"
+      SHOW "You choose Choco Tea. Thanks."
+    ELSE 
+      SHOW "Error: please contact the provider" 
+  END
+*/
+
+let chosen = "Sweet Tea"
+
+switch (chosen) {
+  case "Sweet Tea":
+    console.log(`You choose ${chosen}. Thanks.`)
+    break
+  case "Plain Tea":
+    console.log(`You choose ${chosen}. Thanks.`)
+    break
+  case "Boba":
+    console.log(`You choose ${chosen}. Thanks.`)
+    break
+  case "Milk Coffee":
+    console.log(`You choose ${chosen}. Thanks.`)
+    break
+  case "Choco Tea":
+    console.log(`You choose ${chosen}. Thanks.`)
+    break
+  default:
+    console.log("Error: please contact the provider")
+    break
+}
