e9729e596c2f9064d9de2d81a1e425ea1cd2ffd5
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-06 11:58:20 +0700

    switch case practise

---
+++ switchcase.js
@@ -0,0 +1,14 @@
+
+let drink = 'jack daniels'
+
+switch (drink) {
+  case 'coffee':
+    console.log('I drink coffee')
+    break
+  case 'tea':
+    console.log('I drink tea')
+    break
+  default:
+    console.log('Sorry, I don\'t drink that')
+    break
+}
