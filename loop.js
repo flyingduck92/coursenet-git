0bfbbea1c8e524ea94bc908111689858e2decdc5
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-12 10:17:14 +0700

    add nested loop

--- loop.js
+++ loop.js
@@ -1,22 +1,24 @@
-// for (let i = 0; i < 3; i++) {
-//   console.log(i)
-// }
+/* For loop */
+for (let i = 0; i < 3; i++) {
+  console.log(i)
+}
 
-// let i = 0
-// while (i < 3) {
-//   console.log('Hello !')
-//   i++
-// }
+/* While loop */
+let i = 0
+while (i < 3) {
+  console.log('Hello !')
+  i++
+}
 
-// let i = 0, status = true
-
-// while (_status) {
-//   console.log(i)
-//   i++
-//   if (i > 5) {
-//     _status = false
-//   }
-// }
+/* While-if loop */
+let k = 0, status = true
+while (_status) {
+  console.log(k)
+  i++
+  if (k > 5) {
+    _status = false
+  }
+}
 
 /* Nested loop */
 let temp = ''
