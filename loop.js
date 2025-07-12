a6f688690b9d9264414d3fbf8baefbf4fc30abee
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-12 09:57:46 +0700

    loop - for and while

---
+++ loop.js
@@ -0,0 +1,19 @@
+// for (let i = 0; i < 3; i++) {
+//   console.log(i)
+// }
+
+// let i = 0
+// while (i < 3) {
+//   console.log('Hello !')
+//   i++
+// }
+
+// let i = 0, status = true
+
+// while (_status) {
+//   console.log(i)
+//   i++
+//   if (i > 5) {
+//     _status = false
+//   }
+// }
