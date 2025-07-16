4d24b978585d21a6be2799eec0aa584c283d23a8
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-16 20:19:43 +0700

    closure, class, methods and instances as pillars of javascript

---
+++ three-pillars/ex.js
@@ -0,0 +1,48 @@
+class Bookshelf {
+	constructor() {
+		this.favoriteBooks = []
+	}
+
+	addFavoriteBook(bookName) {
+		if (!bookName.includes("Great")) {
+			this.favoriteBooks.push(bookName)
+		}
+	}
+
+	printFavoriteBooks() {
+		console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`)
+		for (let bookName of this.favoriteBooks) {
+			console.log(bookName)
+		}
+	}
+}
+
+function loadBooks(theBookshelf) {
+	fakeAjax(BOOK_API, function onBooks(bookNames) {
+		for (let bookname of bookNames) {
+			theBookshelf.addFavoriteBook(bookname)
+		}
+		theBookshelf.printFavoriteBooks()
+	})
+}
+
+var BOOK_API = "https://some.url/api"
+
+var myBooks = new Bookshelf()
+loadBooks(myBooks)
+
+
+// ***********************
+
+// NOTE: don't modify this function at all
+function fakeAjax(url, cb) {
+	setTimeout(function fakeLoadingDelay() {
+		cb([
+			"A Song of Ice and Fire",
+			"The Great Gatsby",
+			"Crime & Punishment",
+			"Great Expectations",
+			"You Don't Know JS"
+		])
+	}, 500)
+}
