aacf7ca75cb8f574ed9e44b17dd9c20e6d80e22a
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-19 11:54:42 +0700

    Add delete and edit methods

--- class/factoryClass.js
+++ class/factoryClass.js
@@ -70,13 +70,40 @@ class Supermarket {
         break
     }
   }
+
+  deleteItem(id) {
+    let updated = this.shelves.filter(shelf => shelf.id !== id)
+    this.shelves = updated
+  }
+
+  editItem(id, name, price, stock) {
+    let updated = this.shelves.map(shelf => {
+      if (shelf.id === Number(id)) {
+        shelf.stock = stock
+        shelf.name = name
+        shelf.price = price
+      }
+      return shelf
+    })
+    this.shelves = updated
+  }
 }
 
 const supermarket = new Supermarket()
-supermarket.addItem("Apel Malang", 28000, 10, "Apple")
-supermarket.addItem("Jeruk Medan", 35000, 10, "Orange")
-supermarket.addItem("Pisang Cavendish", 28000, 6, "Banana")
-supermarket.addItem("Jeruk Bali Pamelo", 40000, 8, "Orange")
-supermarket.addItem("Pisang Tanduk", 40000, 8, "Banana")
+supermarket.addItem("Apel Malang", 28000, 10, "Apple") // 1
+supermarket.addItem("Jeruk Medan", 35000, 10, "Orange") // 2
+supermarket.addItem("Cavendish Sunpride", 28000, 6, "Banana") // 3
+supermarket.addItem("Jeruk Bali Pamelo", 40000, 8, "Orange") // 4
+supermarket.addItem("Pisang Tanduk", 40000, 8, "Banana") // 5
 // supermarket.showItems()
+supermarket.showInTable()
+supermarket.deleteItem(2)
+console.log('Id 2 deleted')
+supermarket.deleteItem(4)
+console.log('Id 4 deleted')
+supermarket.showInTable()
+supermarket.editItem(1, "Apel Mandarin", 35000, 15)
+console.log('Id 1 updated')
+supermarket.editItem(5, "Pisang Raja", 25000, 25)
+console.log('Id 5 updated')
 supermarket.showInTable()
