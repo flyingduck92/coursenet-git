c63e7a5d199039ecf92dcc04d6718e0d0cf6928b
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-07-21 23:59:24 +0700

    Refactoring sort and formatItem functionality for better readability

--- mvc_ex/models/Model.js
+++ mvc_ex/models/Model.js
@@ -118,44 +118,21 @@ class ModelWine {
       https://mimo.org/glossary/javascript/sort
     */
     let wines = this.getAll()
+    let message = ''
 
-    // ascending
-    if (params[0].toLowerCase() === 'asc' || params[0].toLowerCase() === 'ascending') {
-      wines.sort((a, b) => b.year - a.year)
-
-      let message = ''
-
-      for (let i = 0; i < wines.length; i++) {
-        let age = new Date().getFullYear() - wines[i].year
-        if (message === '') {
-          message += `${i + 1}. ${wines[i].name} (${age} years)`
-        } else {
-          message += `\n${i + 1}. ${wines[i].name} (${age} years)`
-        }
-      }
-
-      return message
+    switch (params[0].toLowerCase()) {
+      case 'asc':
+      case 'ascending':
+        wines.sort((a, b) => b.year - a.year)
+        message = this.sortFormat(wines)
+        break
+      case 'desc':
+      case 'descending':
+        wines.sort((a, b) => a.year - b.year)
+        message = this.sortFormat(wines)
+        break
     }
-
-    // descending
-    else if (params[0].toLowerCase() === 'desc' || params[0].toLowerCase() === 'descending') {
-      wines.sort((a, b) => a.year - b.year)
-
-      let message = ''
-
-      for (let i = 0; i < wines.length; i++) {
-        let age = new Date().getFullYear() - wines[i].year
-
-        if (message === '') {
-          message += `${i + 1}. ${wines[i].name} (${age} years)`
-        } else {
-          message += `\n${i + 1}. ${wines[i].name} (${age} years)`
-        }
-      }
-
-      return message
-    }
-
+    return message
   }
 
   static formatItem(item) {
@@ -163,13 +140,17 @@ class ModelWine {
     year = Number(year)
 
     switch (type) {
-      case 'R' || 'r':
-      case 'Red' || 'red':
+      case 'R':
+      case 'r':
+      case 'Red':
+      case 'red':
       case 'RED':
         type = 'Red'
         break
-      case 'W' || 'w':
-      case 'White' || 'white':
+      case 'W':
+      case 'w':
+      case 'White':
+      case 'white':
       case 'WHITE':
         type = 'White'
         break
@@ -179,6 +160,21 @@ class ModelWine {
     }
 
     return { name, year, type }
+  }
+
+  static sortFormat(wines) {
+    let message = ''
+
+    for (let i = 0; i < wines.length; i++) {
+      let age = new Date().getFullYear() - wines[i].year
+      if (message === '') {
+        message += `${i + 1}. ${wines[i].name} (${age} years)`
+      } else {
+        message += `\n${i + 1}. ${wines[i].name} (${age} years)`
+      }
+    }
+
+    return message
   }
 
   static save(updatedItems) {
